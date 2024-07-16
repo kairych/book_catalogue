e28f015144f650a034377600f4d8368d1ad023bb
kairat <tussupbekov@gmail.com>
2024-07-16 15:19:16 +0500

    Added features to create review and to add/remove a book to/from favorites. Added corresponding views and urls.

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
-from .views import BookListView, BookDetailView
+from .views import BookListView, BookDetailView, ReviewCreateView, AddToFavoriteView
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', BookListView.as_view(), name='book_list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('books/<int:pk>/reviews/', ReviewCreateView.as_view(), name='review_create'),
+    path('books/<int:pk>/toggle_favorite/', AddToFavoriteView.as_view(), name='toggle_favorite'),
 ]
--- books/views.py
+++ books/views.py
@@ -1,12 +1,15 @@
 from django.db.models import Avg
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework import status
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
 from rest_framework.filters import OrderingFilter
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .filters import BookFilter
-from .models import Book
-from .serializers import BookListSerializer, BookDetailSerializer
+from .models import Book, Review
+from .serializers import BookListSerializer, BookDetailSerializer, ReviewSerializer
 
 
 class BookListView(ListAPIView):
@@ -42,3 +45,50 @@ class BookDetailView(RetrieveAPIView):
         queryset = super().get_queryset()
         queryset = queryset.annotate(average_rating=Avg('reviews__rating')) # Calculate average rating for selected book
         return queryset
+
+
+class ReviewCreateView(CreateAPIView):
+    """
+    Create a new review with book rating.
+    """
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated, ]
+
+    def create(self, request, *args, **kwargs):
+        book_id = self.kwargs.get('pk')
+        try:
+            book = Book.objects.get(pk=book_id)
+        except Book.DoesNotExist:
+            return Response({"error": "Book not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        # Check if the user has already reviewed this book
+        if Review.objects.filter(book=book, author=request.user).exists():
+            return Response({"error": "You have already reviewed this book"}, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(book=book, author=request.user)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class AddToFavoriteView(APIView):
+    """
+    Add or remove a book from favorites.
+    No content should be added to the request.
+    """
+    permission_classes = [IsAuthenticated, ]
+
+    def post(self, request, pk):
+        try:
+            book = Book.objects.get(pk=pk)
+        except Book.DoesNotExist:
+            return Response({"error": "Book not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        user = request.user
+        if book.favorites.filter(id=user.id).exists():
+            book.favorites.remove(user)
+            return Response({"Status": "Removed from favorites"}, status=status.HTTP_200_OK)
+        else:
+            book.favorites.add(user)
+            return Response({"Status": "Added to favorites"}, status=status.HTTP_200_OK)
